d896311e0de765b722cdcb3495b6b1d439b92b4f
Kwame Taylor <maxkwametaylor@gmail.com>
2021-01-08 19:56:58 -0600

    fix bug that dropped release year

--- preprocessing.py
+++ preprocessing.py
@@ -9,13 +9,8 @@ def plots_split(df, target):
     It also splits test, validate, and train into X and y dataframes.
     Returns X_train, y_train, X_validate, y_validate, X_test, y_test, train, validate, test.
     '''
-
     # split df into test (15%) and train_validate (85%)
     train_validate, test = train_test_split(df, test_size=.15, stratify=df['Release Year'], random_state=666)
-
-    # drop column used for stratification
-    train_validate = train_validate.drop(columns=['Release Year'])
-    test = test.drop(columns=['Release Year'])
 
     # split train_validate off into train (82.35% of 85% = 70%) and validate (17.65% of 85% = %15)
     train, validate = train_test_split(train_validate, test_size=.1765, random_state=666)
